1dfd2140f1f86d40a64b343790dd853799aa812b
hudson-newey <grath@duck.com>
2024-10-05 21:46:51 +1000

    qol and bug fixes
    - Ctrl + Shift + F now works in alacritty
    - Splitting the pane now automatically switches to it
    - :Q now acts as :q
    - Add additional commands

--- modules/commands.lua
+++ modules/commands.lua
@@ -14,6 +14,14 @@ local commands = {
 	{ name = "ReloadWindow", command = "windo e", desc = "Developer: Reload Window" },
 
 	{ name = "TogglePrimarySideBarVisibility", command = "Neotree toggle", desc = "View: Toggle Primary Side Bar Visibility" },
+
+	{ name = "Find", command = "Telescope search_history", desc = "Find" },
+
+	{ name = "SearchFindInFiles", command = "Telescope live_grep", desc = "Search: Find in Files" },
+
+	-- I commonly make typos where I make "q" a capital letter
+	-- to fix this, I make an alias to the "q" command
+	{ name = "Q", command = "q", desc = "" },
 };
 
 for _, v in ipairs(commands) do
--- modules/keymap.lua
+++ modules/keymap.lua
@@ -1,42 +1,49 @@
 local keymap = {
-	{ key = "C-P", command = "Telescope find_files" },
-	{ key = "CS-P", command = "Telescope commands" },
+	{ key = "<C-P>", command = "Telescope find_files" },
+	{ key = "<CS-P>", command = "Telescope commands" },
 
-	{ key = "C-O", command = "Telescope projects" },
+	{ key = "<C-O>", command = "Telescope projects" },
 
-	{ key = "C-B", command = "Neotree toggle" },
-	{ key = "CS-E", command = "Neotree reveal" },
+	{ key = "<CS-O>", command = "Telescope lsp_document_symbols" },
 
-	{ key = "C-F", command = "Telescope search_history" },
-	{ key = "CS-F", command = "Telescope live_grep" },
+	{ key = "<C-B>", command = "Neotree toggle", insert = true },
+	{ key = "<CS-E>", command = "Neotree reveal", insert = true },
 
-	{ key = "CS-X", command = "Mason" },
+	{ key = "<C-F>", command = "Telescope current_buffer_fuzzy_find" },
+	{ key = "<CS-F>", command = "Telescope live_grep" },
 
-	{ key = "C-S", command = "w" },
-	{ key = "C-Z", command = "undo" },
-	{ key = "CS-Z", command = "redo" },
+	{ key = "<CS-X>", command = "Mason" },
 
-	{ key = "C-\\", command = "vs" },
-	{ key = "CS-\\", command = "split" },
+	{ key = "<C-S>", command = "w", insert = true },
+	{ key = "<C-Z>", command = "undo", insert = true },
+	{ key = "<CS-Z>", command = "redo", insert = true },
 
-	{ key = "C-TAB", command = "BufferNext" },
-	{ key = "CS-TAB", command = "BufferPrevious" },
+	{ key = "<C-\\>", command = "vs | wincmd l", insert = true },
+	{ key = "<CS-\\>", command = "split | wincmd j", insert = true },
 
-	{ key = "ESC", command = "noh" },
+	{ key = "<C-TAB>", command = "BufferNext" },
+	{ key = "<CS-TAB>", command = "BufferPrevious" },
 
-	{ key = "C-Q", command = "BufferClose" },
+	{ key = "<ESC>", command = "noh" },
 
-	{ key = "C-]", command = "BufferNext" },
-	{ key = "CS-]", command = "BufferPrevious" },
-	{ key = "C-Q", command = "BufferClose" },
+	{ key = "<C-Q>", command = "BufferClose" },
 
-	{ key = "C-j", command = "lua require('nvterm.terminal').toggle 'horizontal'" },
-	{ key = "C-`", command = "lua require('nvterm.terminal').toggle 'horizontal'" },
-	{ key = "CS-`", command = "lua require('nvterm.terminal').toggle 'horizontal'" },
+	{ key = "<C-]>", command = "BufferNext" },
+	{ key = "<CS-]>", command = "BufferPrevious" },
+	{ key = "<C-Q>", command = "BufferClose" },
+
+	{ key = "<C-j>", command = "lua require('nvterm.terminal').toggle 'horizontal'" },
+	{ key = "<C-`>", command = "lua require('nvterm.terminal').toggle 'horizontal'" },
+	{ key = "<CS-`>", command = "lua require('nvterm.terminal').toggle 'horizontal'" },
 }
 
 for _, v in ipairs(keymap) do
-	vim.api.nvim_set_keymap("n", "<" .. v.key .. ">", ":" .. v.command .. "<CR>", { noremap = true, silent = true })
+	vim.api.nvim_set_keymap("n", v.key, ":" .. v.command .. "<CR>", { noremap = true, silent = true })
+
+	if v.insert then
+		-- exit insert mode, execute the command, then return to insert mode
+		vim.api.nvim_set_keymap("i", v.key, "<ESC>:" .. v.command .. "<CR>i", { noremap = true, silent = true })
+	end
 end
 
 vim.cmd "vnoremap < <gv"
@@ -45,3 +52,13 @@ vim.cmd "vnoremap > >gv"
 -- I remap Ctrl+D to Ctrl+N because the multi word select extension default to Ctrl+N
 -- This is not what I like, therefore, I"ve rebinded it to Ctrl+D (same as VsCode and Sublime Text)
 vim.cmd "nnoremap <C-d> <C-n>"
+
+-- map tab to a tab completion so that I can press tab to complete an auto
+-- complete query
+vim.cmd "inoremap <expr> <TAB> pumvisible() ? '<C-y>' : '<TAB>'"
+
+-- in alacritty, you are not able to overwrite ctrl + shift + F
+-- to fix this, I have seen a hacky stack overflow solution that prints invisible unicode characters
+-- and uses that as a signal to trigger the ctrl + shift + F shortcut
+-- see: https://stackoverflow.com/a/78694090
+vim.keymap.set("n", "♠", ":Telescope live_grep<CR>")
