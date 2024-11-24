f56d6f41ce6965cd80934086c014f17b10f8b110
hudson-newey <grath@duck.com>
2024-11-24 20:21:47 +1000

    Fix starting new instance through shell

--- install.py
+++ install.py
@@ -46,20 +46,20 @@ fi
 
 def wsl_starter(runtime: str) -> str:
     return f"""{common_starter(runtime)}
-{runtime}/setup.sh {runtime}/init.lua $@
+{runtime}/setup.sh {runtime}/init.lua $@ & > /dev/null
 """
 
 
 # we have to override the $SHELL environment variable to support underline errors
 def alacritty_starter(runtime: str) -> str:
     return f"""{common_starter(runtime)}
-alacritty -T "$startPath - NovaVim" --class nvim --config-file {runtime}/configs/alacritty.toml -e {runtime}/setup.sh {runtime}/init.lua $@
+alacritty -T "$startPath - NovaVim" --class nvim --config-file {runtime}/configs/alacritty.toml -e {runtime}/setup.sh {runtime}/init.lua $@ & > /dev/null
 """
 
 
 def xterm_starter(runtime: str) -> str:
     return f"""{common_starter(runtime)}
-xterm +sb -bg black -fg white -fa "M+1Code Nerd Font Mono" -fs 10 -title "$startPath - NovaVim" -name {runtime}/configs/.Xresources -e {runtime}/setup.sh {runtime}/init.lua $@
+xterm +sb -bg black -fg white -fa "M+1Code Nerd Font Mono" -fs 10 -title "$startPath - NovaVim" -name {runtime}/configs/.Xresources -e {runtime}/setup.sh {runtime}/init.lua $@ & > /dev/null
 xrdb -query | grep -q 'XTerm/*vt100/.translationsa' |> /dev/null
 if [ $? != 0 ]; then xterm -e xrdb -merge ./configs/.Xresources; fi
 """
