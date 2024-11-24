381ce64e48b552a18bd1869c8382847f2b5eb724
hudson-newey <grath@duck.com>
2024-11-24 16:15:31 +1000

    Fix terminal titles

--- install.py
+++ install.py
@@ -2,45 +2,60 @@
 import os
 import sys
 import stat
-import subprocess
 import shutil
+
 
 def is_wsl() -> bool:
     try:
         with open("/proc/version", "r") as f:
             return "microsoft" in f.read().lower()
-    except:
+    except Exception:
         return False
+
 
 def is_windows() -> bool:
     return sys.platform == "win32"
 
+
 def has_alacritty() -> bool:
     return shutil.which("alacritty") is not None
+
 
 def has_xterm() -> bool:
     return shutil.which("xterm") is not None
 
+
 def common_starter(runtime: str) -> str:
-    return f"""#!/usr/bin/env bash
+    return """#!/usr/bin/env bash
 declare startPath="."
-if [ $# -eq 0 ]; then
-	startPath="{runtime}"
+declare currentDir=$(pwd)
+
+# if it is an absolute path, we do not want to use "realpath"
+if [[ $1 = /* ]]; then
+    startPath="$1"
 else
-	startPath="{runtime}/$1"
+    # if it is a relative path we can use realpath
+    if [ $# -eq 0 ]; then
+        startPath="$(realpath $currentDir)"
+    else
+        startPath="$(realpath $currentDir/$1)"
+    fi
 fi
 """
+
 
 def wsl_starter(runtime: str) -> str:
     return f"""{common_starter(runtime)}
 {runtime}/setup.sh {runtime}/init.lua $@
 """
 
+
 # we have to override the $SHELL environment variable to support underline errors
 def alacritty_starter(runtime: str) -> str:
     return f"""{common_starter(runtime)}
 alacritty -T "$startPath - NovaVim" --class nvim --config-file {runtime}/configs/alacritty.toml -e {runtime}/setup.sh {runtime}/init.lua $@
 """
+
 
 def xterm_starter(runtime: str) -> str:
     return f"""{common_starter(runtime)}
@@ -49,12 +64,14 @@ xrdb -query | grep -q 'XTerm/*vt100/.translationsa' |> /dev/null
 if [ $? != 0 ]; then xterm -e xrdb -merge ./configs/.Xresources; fi
 """
 
+
 def init_module(runtime: str) -> str:
     return f"""
 package.path = '{runtime}/?.lua;'..package.path
 package.path = '{runtime}/modules/?.lua;'..package.path
 require('setup')
 """
+
 
 def main() -> None:
     home = os.getenv("HOME")
@@ -92,6 +109,7 @@ def main() -> None:
     print(f"Creating symlink in {starter_path}")
     os.chmod(starter_path, new_permissions)
     os.symlink(starter_path, bin_target)
+
 
 if __name__ == "__main__":
     main()
