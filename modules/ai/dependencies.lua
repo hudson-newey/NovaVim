0da8dd0ece7ee21ab76aa6109a7703b95fe206b2
hudson-newey <hudsonnewey@gmail.com>
2025-06-15 21:19:52 +1000

    Persist better comments through colorscheme changes

--- modules/ai/dependencies.lua
+++ modules/ai/dependencies.lua
@@ -1,8 +1,5 @@
 local dependency_table = {
 	"David-Kunz/gen.nvim",
-
-	-- TODO: I might want to replace gen.nvim with avante.nvim
-	-- "yetone/avante.nvim",
 }
 
 return dependency_table
--- modules/highlights.lua
+++ modules/highlights.lua
@@ -1,14 +1,39 @@
 -- used to highlight certain words in different colours
 -- eg. I want to highlight todos and conform to the better comments standard
 
-vim.cmd "hi TodoColor ctermbg=205 guibg=#FF8C00 guifg=black ctermfg=black"
-vim.cmd "hi AlertColor ctermbg=205 guibg=#FF2D00 guifg=black ctermfg=black"
-vim.cmd "hi InfoColor ctermbg=205 guibg=#3498DB guifg=black ctermfg=black"
-vim.cmd "hi ImportantColor ctermbg=205 guibg=#98C379 guifg=black ctermfg=black"
-
 -- Conforms to the better-comments format by aaron bond
 -- Under the MIT license
 function AddHighlights()
+	vim.fn.clearmatches()
+
+	vim.api.nvim_set_hl(0, "TodoColor", {
+		bg = "#FF8C00", -- guibg
+		fg = "black",   -- guifg
+		ctermbg = 205,  -- ctermbg
+		ctermfg = 16,   -- ctermfg (16 is typically black in 256-color terminals)
+	})
+
+	vim.api.nvim_set_hl(0, "AlertColor", {
+		bg = "#FF2D00",
+		fg = "black",
+		ctermbg = 205,
+		ctermfg = 16,
+	})
+
+	vim.api.nvim_set_hl(0, "InfoColor", {
+		bg = "#3498DB",
+		fg = "black",
+		ctermbg = 205,
+		ctermfg = 16,
+	})
+
+	vim.api.nvim_set_hl(0, "ImportantColor", {
+		bg = "#98C379",
+		fg = "black",
+		ctermbg = 205,
+		ctermfg = 16,
+	})
+
 	vim.fn.matchadd("TodoColor", "TODO")
 	vim.fn.matchadd("TodoColor", "TODO:")
 
@@ -22,5 +47,6 @@ function AddHighlights()
 	--vim.fn.matchadd("ImporantColor", "--\*")
 end
 
-vim.cmd("autocmd BufRead * lua AddHighlights()")
-vim.cmd("autocmd WinEnter * lua AddHighlights()")
+vim.api.nvim_create_autocmd("ColorScheme", { callback = AddHighlights })
+vim.api.nvim_create_autocmd("BufRead", { callback = AddHighlights })
+vim.api.nvim_create_autocmd("WinEnter", { callback = AddHighlights })
