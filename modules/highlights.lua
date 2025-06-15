9891c4b3fa8699eb0d81458e47a932735df675e5
hudson-newey <hudsonnewey@gmail.com>
2025-06-15 21:39:57 +1000

    Improve comment highlights

--- modules/highlights.lua
+++ modules/highlights.lua
@@ -7,44 +7,43 @@ function AddHighlights()
 	vim.fn.clearmatches()
 
 	vim.api.nvim_set_hl(0, "TodoColor", {
-		bg = "#FF8C00", -- guibg
-		fg = "black",   -- guifg
+		bg = "black",   -- guifg
+		fg = "#FF8C00", -- guibg
 		ctermbg = 205,  -- ctermbg
 		ctermfg = 16,   -- ctermfg (16 is typically black in 256-color terminals)
 	})
 
 	vim.api.nvim_set_hl(0, "AlertColor", {
-		bg = "#FF2D00",
-		fg = "black",
+		bg = "black",
+		fg = "#FF2D00",
 		ctermbg = 205,
 		ctermfg = 16,
 	})
 
 	vim.api.nvim_set_hl(0, "InfoColor", {
-		bg = "#3498DB",
-		fg = "black",
+		bg = "black",
+		fg = "#3498DB",
 		ctermbg = 205,
 		ctermfg = 16,
 	})
 
 	vim.api.nvim_set_hl(0, "ImportantColor", {
-		bg = "#98C379",
-		fg = "black",
+		bg = "black",
+		fg = "#98C379",
 		ctermbg = 205,
 		ctermfg = 16,
 	})
 
-	vim.fn.matchadd("TodoColor", "TODO")
-	vim.fn.matchadd("TodoColor", "TODO:")
+	vim.fn.matchadd("TodoColor", "TODO.*")
 
-	vim.fn.matchadd("AlertColor", "//!")
-	vim.fn.matchadd("AlertColor", "--!")
+	vim.fn.matchadd("AlertColor", "//!.*")
+	vim.fn.matchadd("AlertColor", "--!.*")
 
-	vim.fn.matchadd("InfoColor", "//?")
-	vim.fn.matchadd("InfoColor", "--?")
+	vim.fn.matchadd("InfoColor", "//?.*")
+	vim.fn.matchadd("InfoColor", "--?.*")
 
-	--vim.fn.matchadd("ImporantColor", "//\*")
-	--vim.fn.matchadd("ImporantColor", "--\*")
+	-- vim.fn.matchadd("ImporantColor", "//\\*")
+	-- vim.fn.matchadd("ImporantColor", "--\\*")
 end
 
 vim.api.nvim_create_autocmd("ColorScheme", { callback = AddHighlights })
