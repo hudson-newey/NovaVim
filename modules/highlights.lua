746e5f5f0463a86e215cbe373c9e7a5b1fdc23d0
hudson-newey <hudsonnewey@gmail.com>
2025-06-15 21:50:09 +1000

    Improve comment highlighting code

--- modules/highlights.lua
+++ modules/highlights.lua
@@ -1,9 +1,18 @@
 -- used to highlight certain words in different colours
 -- eg. I want to highlight todos and conform to the better comments standard
 
+local function register_comment_highlight(name, keyword)
+	local matcher_prefixes = { "//", "--" }
+
+	for _, value in ipairs(matcher_prefixes) do
+		vim.fn.matchadd(name, value .. keyword .. ".*")
+		vim.fn.matchadd(name, value .. " " .. keyword .. ".*")
+	end
+end
+
 -- Conforms to the better-comments format by aaron bond
 -- Under the MIT license
-function AddHighlights()
+local function add_highlights()
 	vim.fn.clearmatches()
 
 	vim.api.nvim_set_hl(0, "TodoColor", {
@@ -34,18 +43,12 @@ function AddHighlights()
 		ctermfg = 16,
 	})
 
-	vim.fn.matchadd("TodoColor", "TODO.*")
-
-	vim.fn.matchadd("AlertColor", "//!.*")
-	vim.fn.matchadd("AlertColor", "--!.*")
-
-	vim.fn.matchadd("InfoColor", "//?.*")
-	vim.fn.matchadd("InfoColor", "--?.*")
-
-	-- vim.fn.matchadd("ImporantColor", "//\\*")
-	-- vim.fn.matchadd("ImporantColor", "--\\*")
+	register_comment_highlight("TodoColor", "TODO")
+	register_comment_highlight("AlertColor", "!")
+	register_comment_highlight("InfoColor", "?")
+	-- register_highlights("ImporantColor", "\\*")
 end
 
-vim.api.nvim_create_autocmd("ColorScheme", { callback = AddHighlights })
-vim.api.nvim_create_autocmd("BufRead", { callback = AddHighlights })
-vim.api.nvim_create_autocmd("WinEnter", { callback = AddHighlights })
+vim.api.nvim_create_autocmd("ColorScheme", { callback = add_highlights })
+vim.api.nvim_create_autocmd("BufRead", { callback = add_highlights })
+vim.api.nvim_create_autocmd("WinEnter", { callback = add_highlights })
